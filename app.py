49807b3a3eb846b4a2a51dbf2a323dcfc1d2a827
Rodolfo Ferro <rodolfoferro@Rodolfos-MacBook-Pro.local>
2021-03-19 12:19:22 -0600

    Initial commit. :fire:

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask import render_template
+from generator import *
+
+
+# Simple Flask app
+app = Flask(__name__)
+
+# Create a model
+model = generate_model('docs/rlv_material_unam.txt')
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/generate", methods=['GET', 'POST'])
+def generate():
+    text = model.make_short_sentence(250)
+    print(text)
+    return render_template('index.html', text=text)
+
+
+# Main:
+if __name__ == '__main__':
+    app.run(debug=True)
